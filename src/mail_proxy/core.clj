99930f288d178181b92633bfed870cd41a6c68c8
llj098 <llj098@gmail.com>
2013-02-07 21:21:42 +0800

    add forward

--- src/mail_proxy/core.clj
+++ src/mail_proxy/core.clj
@@ -7,8 +7,11 @@
     Executors]
    [org.jboss.netty.channel
     ChannelPipeline
+    ChannelPipelineFactory
+    Channels
     ChannelUpstreamHandler
     SimpleChannelUpstreamHandler
+    DefaultChannelFuture
     ChannelEvent]
    [javax.net.ssl
     X509TrustManager
@@ -16,9 +19,11 @@
    [org.jboss.netty.channel.group
     DefaultChannelGroup]
    [org.jboss.netty.channel.socket.nio
-    NioClientSocketChannelFactory]
+    NioClientSocketChannelFactory
+    NioServerSocketChannelFactory]
    [org.jboss.netty.bootstrap
-    ClientBootstrap]
+    ClientBootstrap
+    ServerBootstrap]
    [org.jboss.netty.handler.ssl
     SslHandler]
    [org.jboss.netty.handler.codec.frame
@@ -34,12 +39,26 @@
     (checkServerTrusted [_ _ _])
     (getAcceptedIssuers [_])))
 
-(def message-handler
+(def client-handler
   (proxy [SimpleChannelUpstreamHandler] []
     (handleUpstream [ctx e] (proxy-super handleUpstream ctx e))
-    (channelConnected [ctx e] )
-    (messageReceived [ctx e] (println (.getMessage e)))
-    (exceptionCaught [ctx e] (do (println (.getCause e)) (-> e .getChannel .close)))))
+    (channelDisconnected [ctx e]
+      (let [cch (.getChannel e)
+            sch (.getAttachment cch)]
+        (do
+          (.close cch)
+          (.close sch))))
+    (messageReceived [ctx e]
+      (let [sch (-> ctx .getChannel .getAttachment)]
+        (.write sch (.getMessage e))))
+    (exceptionCaught [ctx e]
+      (do
+        (println (.getCause e))
+        (let [cch (.getChannel e)
+              sch (.getAttachment cch)]
+          (do
+            (.close sch)
+            (.close cch)))))))
 
 (defn create-ssl-handler
   [{:keys [server-name server-port ignore-ssl-certs?]}]
@@ -56,23 +75,63 @@
     (.setCloseOnSSLException true)))
 
 
-(defn -main[]
+(defn create-client[]
   (let [client (ClientBootstrap. (NioClientSocketChannelFactory.
                                   (Executors/newCachedThreadPool)
                                   (Executors/newCachedThreadPool)))
         pipe (.getPipeline client)]
     (do
+      (.setOption client "connectTimeoutMillis" 10000)
       (doto pipe
         (.addLast "ssl", (create-ssl-handler {:server-name "pop3.feinno.com" :server-port 995}))
-        (.addLast "framer" (DelimiterBasedFrameDecoder. 8192 (Delimiters/lineDelimiter)))
-        (.addLast "decoder" (StringDecoder.))
-        (.addLast "encoder" (StringEncoder.))
-        (.addLast "handler" message-handler))
-      (let [fu (-> client (.connect (InetSocketAddress. "pop3.feinno.com" 995)))
-            ch (-> fu .awaitUninterruptibly .getChannel)
-            in (BufferedReader. (InputStreamReader. System/in))]
-            (while true
-              (.write ch (str (.readLine in) "\r\n")))))))
+;        (.addLast "framer" (DelimiterBasedFrameDecoder. 8192 (Delimiters/lineDelimiter)))
+;        (.addLast "decoder" (StringDecoder.))
+;        (.addLast "encoder" (StringEncoder.))
+        (.addLast "handler" client-handler))
+      client)))
 
+(def message-handler
+  (proxy [SimpleChannelUpstreamHandler] []
+    (handleUpstream [ctx e] (proxy-super handleUpstream ctx e))
+    (channelConnected [ctx e] )
+    (messageReceived [ctx e] (println (.getMessage e)))
+    (exceptionCaught [ctx e] (do (println (.getCause e)) (-> e .getChannel .close)))))
 
+(def server-handler
+  (proxy [SimpleChannelUpstreamHandler] []
+    (messageReceived [ctx e]
+      (.write (.getAttachment ctx) (.getMessage e)))
+    (channelDisconnected [ctx e]
+      (let [cch (.getAttachment ctx)
+            sch (.getChannel e)]
+        (do
+          (.close cch)
+          (.close sch))))
+    (channelConnected [ctx e]
+      (let [client (create-client)
+            fu (.connect client (InetSocketAddress. "pop3.feinno.com" 995))
+            cch (-> fu .awaitUninterruptibly .getChannel)]
+        (do 
+          (.setAttachment ctx cch)
+          (.setAttachment cch (.getChannel e)))))
+    (exceptionCaught [ctx e]
+      (do
+        (println (.getCause e))
+        (let [sch (.getChannel e)
+              cch (.getAttachment ctx)]
+          (do
+            (.close cch)
+            (.close sch)))))))
 
+(defn start-server [port]
+  (let [svr (ServerBootstrap. (NioServerSocketChannelFactory.
+                               (Executors/newCachedThreadPool)
+                               (Executors/newCachedThreadPool)))
+        pipe (.getPipeline svr)]
+    (do
+      (DefaultChannelFuture/setUseDeadLockChecker false)
+      (.addLast pipe "handler" server-handler)
+      (.bind svr (InetSocketAddress. port)))))
+
+(defn -main[]
+  (start-server 9999))
